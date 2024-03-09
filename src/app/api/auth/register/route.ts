ab6cab3058b05ef83786f68965399b25178585f2
yaredow <yaredyilma11@gmail.com>
2024-03-09 21:38:16 +0200

    add the user along side the response

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request, res: Response) {
     });
 
     NextResponse.json(
-      { message: "Account created successfully" },
+      { message: "Account created successfully", data: newUser },
       { status: 201 },
     );
   } catch (err) {
