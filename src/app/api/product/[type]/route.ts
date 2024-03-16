62fe65422a93e45984b01678c255b089c72a2323
yaredow <yaredyilma11@gmail.com>
2024-03-16 21:32:34 +0200

    remove all the aggegation folders

--- src/app/api/product/[type]/route.ts
+++ src/app/api/product/[type]/route.ts
@@ -3,9 +3,13 @@ import connectMongoDB from "@/lib/utils/mongo/db";
 import Product from "@/models/productModel";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const type = request.url.slice(request.url.lastIndexOf("/") + 1);
+  console.log(request);
+
   const minUnitSold = 10;
   await connectMongoDB();
+
   const bestSellingProducts = await Product.aggregate([
     {
       $match: { unitsSold: { $gte: minUnitSold } },
