dd5b6a8290c1d698d7a815c7e304271922e23b78
yaredow <yaredyilma11@gmail.com>
2024-03-16 21:44:03 +0200

    finsih the product aggregation function

--- src/app/api/product/[type]/route.ts
+++ src/app/api/product/[type]/route.ts
@@ -7,23 +7,67 @@ export async function GET(request: Request) {
   const type = request.url.slice(request.url.lastIndexOf("/") + 1);
   console.log(request);
 
-  const minUnitSold = 10;
   await connectMongoDB();
+  switch (type) {
+    case "":
+      const products = await Product.find();
+      return NextResponse.json({ products }, { status: 200 });
 
-  const bestSellingProducts = await Product.aggregate([
-    {
-      $match: { unitsSold: { $gte: minUnitSold } },
-    },
-    {
-      $group: groupObject,
-    },
-    {
-      $limit: 8,
-    },
-    {
-      $sort: { unitsSold: -1 },
-    },
-  ]);
+    case "trending":
+      const minUnitSold = 10;
+      const trendingProducts = await Product.aggregate([
+        {
+          $match: { unitsSold: { $gte: minUnitSold } },
+        },
+        {
+          $group: groupObject,
+        },
+        {
+          $limit: 8,
+        },
+        {
+          $sort: { unitsSold: -1 },
+        },
+      ]);
 
-  return NextResponse.json({ bestSellingProducts }, { status: 200 });
+      return NextResponse.json({ trendingProducts }, { status: 200 });
+
+    case "featured":
+      const featuredProducts = await Product.aggregate([
+        {
+          $match: {
+            isFeatured: true,
+            stockQuantity: { $gt: 0 },
+          },
+        },
+        {
+          $group: groupObject,
+        },
+      ]);
+
+      return NextResponse.json({ featuredProducts }, { status: 200 });
+
+    case "new-arrival":
+      const daysAgo = 30;
+      const newArrivalProducts = await Product.aggregate([
+        {
+          $match: {
+            productAddedDate: {
+              $gte: new Date(Date.now() - daysAgo * 24 * 60 * 60 * 1000),
+            },
+          },
+        },
+        {
+          $group: groupObject,
+        },
+        {
+          $sort: { productAddedDate: -1 },
+        },
+        {
+          $limit: 8,
+        },
+      ]);
+
+      return NextResponse.json({ newArrivalProducts }, { status: 200 });
+  }
 }
