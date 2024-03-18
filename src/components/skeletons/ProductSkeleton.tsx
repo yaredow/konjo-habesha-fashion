aac368d1c44bfc615f8c9621602eb12af7196db6
yaredow <yaredyilma11@gmail.com>
2024-03-18 19:16:13 +0200

    remove the fallback

--- src/components/skeletons/ProductSkeleton.tsx
+++ src/components/skeletons/ProductSkeleton.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from "../ui/skeleton";
 
 export function ProductSkeleton() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-44 w-[250px] rounded-xl" />
+    <div className="flex flex-col space-y-4">
+      <Skeleton className="h-[250px] w-[200px] rounded-xl" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
   );
