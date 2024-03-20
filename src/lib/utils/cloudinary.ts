78384113624dcc479f04a2485b0b56396c05a317
yaredow <yaredyilma11@gmail.com>
2024-03-20 21:55:25 +0200

    remove cloudinary config

--- src/lib/utils/cloudinary.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME as string,
-  api_key: process.env.CLOUDINARY_API_KEY as string,
-  api_secret: process.env.CLOUDINARY_API_SECRET as string,
-});
-
-export default cloudinary;
