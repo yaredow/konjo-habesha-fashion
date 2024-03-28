6b0fe28ccb6a3bec7fcf7fa308e75c8449f98731
yaredow <yaredyilma11@gmail.com>
2024-03-28 16:27:57 +0200

    finish the error component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,24 +1,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-function error({ error, reset }: { error: Error; reset: () => void }) {
-  const router = useRouter();
+function error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div className=" flex items-center justify-between">
-      <h2>Something went wrong!</h2>
-      <h1>{error.message}</h1>
-      <p>Please try againor contact support if the problem persist</p>
-      <div className=" flex flex-row gap-8">
+    <div className=" grid  items-center gap-20 px-6 py-28">
+      <div className=" flex flex-col items-center justify-center gap-8">
+        <h2 className=" text-base font-semibold tracking-tight">
+          Something went wrong!
+        </h2>
+        <h1 className=" text-3xl font-bold ">{error.message}</h1>
+        <p>Please try again or contact support if the problem persist</p>
+      </div>
+      <div className=" flex flex-row items-center justify-center gap-8">
         <Button onClick={() => reset()}>Try again</Button>
-        <Button variant="outline" onClick={() => router.replace("/")}></Button>
+        <Button variant="outline">
+          <Link href="/">Go back home</Link>
+        </Button>
       </div>
     </div>
   );
