27dcaa3927b1a48124a1d981906d6c2daab49378
yaredow <yaredyilma11@gmail.com>
2024-04-14 16:16:22 +0200

    key error in the strip config

--- src/app/api/checkout/webhook/route.ts
+++ src/app/api/checkout/webhook/route.ts
@@ -3,44 +3,37 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2024-04-10",
-  typescript: true,
 });
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
 export async function POST(req: NextRequest) {
   let event: Stripe.Event;
+  const rawBody = await req.text();
+  const sig = req.headers.get("stripe-signature");
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
   try {
     event = stripe.webhooks.constructEvent(
-      await req.text(),
-      req.headers.get("stripe-signature") as string,
-      process.env.STRIPE_WEBHOOK_SECRET as string,
+      rawBody,
+      sig as string,
+      endpointSecret as string,
     );
-
-    // Handle the event
-    switch (event.type) {
-      case "checkout.session.completed":
-        const session = event.data.object as Stripe.Checkout.Session;
-        console.log(`Checkout session ${session.id} completed.`);
-        // Add your business logic here
-        break;
-      // Add more cases for other event types you want to handle
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    // Return a 200 response to acknowledge receipt of the event
-    return new NextResponse(null, { status: 200 });
   } catch (err: any) {
     console.error(err);
-    return NextResponse.json(
-      { message: "Internal Server Error" },
-      { status: 500 },
-    );
+    return NextResponse.json({ error: err.message }, { status: 400 });
   }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object as Stripe.Checkout.Session;
+      console.log(`Checkout session ${session.id} completed.`);
+      // Add your business logic here
+      break;
+    // Add more cases for other event types you want to handle
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  return new NextResponse(null, { status: 200 });
 }
