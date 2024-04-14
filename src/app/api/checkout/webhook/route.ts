f81eaf35de93ee0fd94a5a9f566be47b0f9ef7ca
yaredow <yaredyilma11@gmail.com>
2024-04-14 12:55:42 +0200

    finish configuring stripe webhook locally

--- src/app/api/checkout/webhook/route.ts
+++ src/app/api/checkout/webhook/route.ts
@@ -1,4 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECREAT_KEY as string);
-export async function POST() {}
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2024-04-10",
+  typescript: true,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: NextRequest) {
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string,
+    );
+
+    // Handle the event
+    switch (event.type) {
+      case "checkout.session.completed":
+        const session = event.data.object as Stripe.Checkout.Session;
+        console.log(`Checkout session ${session.id} completed.`);
+        // Add your business logic here
+        break;
+      // Add more cases for other event types you want to handle
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    return new NextResponse(null, { status: 200 });
+  } catch (err: any) {
+    console.error(err);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
