d35f62032fddc9a182f6fe7456a4ab10ea9e3922
yaredow <yaredyilma11@gmail.com>
2024-04-15 13:40:03 +0300

    refactored the checkout function to recive data

--- src/components/checkout/CheckoutButton.tsx
+++ src/components/checkout/CheckoutButton.tsx
@@ -3,13 +3,14 @@
 import { useSession } from "next-auth/react";
 import { Button } from "../ui/button";
 import { getCheckoutSessionUrlAction } from "@/server/actions/checkout/getCheckoutSessionUrlAction";
+import { CartItem } from "@/types/product";
 
 const CheckoutButton = () => {
   const { data: session } = useSession();
   const cartItems = JSON.parse(localStorage.getItem("cartItems")!);
   const user = session?.user;
 
-  const handleCheckout = async () => {
+  const handleCheckout = async (cartItems: CartItem[], user: any) => {
     const formData = new FormData();
     formData.append("cartItems", JSON.stringify(cartItems));
     formData.append("user", JSON.stringify(user));
@@ -25,7 +26,7 @@ const CheckoutButton = () => {
     <Button
       variant="default"
       disabled={session?.user ? false : true}
-      onClick={() => handleCheckout()}
+      onClick={() => handleCheckout(cartItems, user)}
     >
       {session?.user ? "Check out" : "Login to checkout"}
     </Button>
