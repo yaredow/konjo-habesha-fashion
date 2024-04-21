36d8e53f73fd019570a8e08fcc51f02be6a0403d
yaredow <yaredyilma11@gmail.com>
2024-04-21 13:04:04 +0300

    create a review model

---
+++ src/models/reviewModel.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema({
+  review: {
+    type: String,
+    required: [true, "Review can not be empty!"],
+  },
+  title: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Tour",
+    required: [true, "Review must belong to a tour."],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Product",
+    required: [true, "Review must belong to a product"],
+  },
+  order: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Order",
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Review = mongoose.models.Review || mongoose.model("Review", reviewSchema);
+
+export default Review;
