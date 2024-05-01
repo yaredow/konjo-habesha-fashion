778eb5c6680351ee491fe8d44469936751b5d79d
yaredow <yaredyilma11@gmail.com>
2024-05-01 09:50:21 +0300

    remove the content of uploadthing

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/images/logo/logo.png",
+  },
   adapter: PrismaAdapter(prisma),
   providers: [Google, Facebook],
 });
