63e304aed1dca8604d62224e6089de0eaba42ef2
yaredow <yaredyilma11@gmail.com>
2024-05-05 07:52:39 +0300

    refactored the login page with card wrapper

--- src/app/auth/(auth)/signin/page.tsx
+++ src/app/auth/(auth)/signin/page.tsx
@@ -11,41 +11,14 @@ import FacebookSigninButton from "@/components/FacebookSigninButton";
 import LoginForm from "@/components/forms/LoginForm";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import CardWrapper from "@/components/CardWrapper";
 
 export default function page() {
   return (
     <main className=" my-auto flex items-center justify-center">
-      <Card className=" mx-auto max-w-[36rem] flex-grow items-center p-6">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <LoginForm />
-          <div className=" mx-4 my-4 flex flex-row justify-between text-sm ">
-            <Button variant="link">
-              <Link href="/auth/signup">Register here</Link>
-            </Button>
-            <Button variant="link">
-              <Link href="/auth/forget-password">Forgot your password</Link>
-            </Button>
-          </div>
-        </CardContent>
-        <CardFooter className=" grid grid-rows-1">
-          <div className="mb-8 mt-2 flex flex-grow items-center">
-            <hr className="border-1 w-full rounded-full" />
-            <div className="mx-4 flex-none text-xs font-normal">
-              OR LOGIN WITH
-            </div>
-            <hr className="border-1 w-full rounded-full" />
-          </div>
-
-          <div className="flex flex-col gap-4 md:flex-row">
-            <GoogleButton />
-
-            <FacebookSigninButton />
-          </div>
-        </CardFooter>
-      </Card>
+      <CardWrapper headerLabel="Login" isLogin="true" showSocial={true}>
+        <LoginForm />
+      </CardWrapper>
     </main>
   );
 }
--- src/components/CardWrapper.tsx
+++ src/components/CardWrapper.tsx
@@ -1,13 +1,24 @@
 "use cclient";
 
-import { Card } from "./ui/card";
+import Link from "next/link";
+import FacebookSigninButton from "./FacebookSigninButton";
+import GoogleButton from "./GoogleSigninButton";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 type CardWrapperProps = {
   children: React.ReactNode;
   headerLabel: string;
   backButtonLabel: string;
   backButtonHref: string;
-  showSocial: boolean;
+  showSocial?: boolean;
+  isLogin?: string;
 };
 
 export default function CardWrapper({
@@ -16,6 +27,41 @@ export default function CardWrapper({
   backButtonHref,
   backButtonLabel,
   showSocial,
+  isLogin,
 }: CardWrapperProps) {
-  return <Card className=" w-[400px] shadow-md"></Card>;
+  return (
+    <Card className=" w-[400px] shadow-md">
+      <CardHeader>
+        <CardTitle>{headerLabel}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>{children}</div>
+        {isLogin && (
+          <div className=" mx-4 my-4 flex flex-row justify-between text-sm ">
+            <Button variant="link">
+              <Link href="/auth/signup">Register here</Link>
+            </Button>
+            <Button variant="link">
+              <Link href="/auth/forget-password">Forgot your password</Link>
+            </Button>
+          </div>
+        )}
+      </CardContent>
+      {showSocial && (
+        <CardFooter>
+          <div className="mb-8 mt-2 flex flex-grow items-center">
+            <hr className="border-1 w-full rounded-full" />
+            <div className="mx-4 flex-none text-xs font-normal">
+              OR LOGIN WITH
+            </div>
+            <hr className="border-1 w-full rounded-full" />
+          </div>
+          <div className="flex flex-col gap-4 md:flex-row">
+            <GoogleButton />
+            <FacebookSigninButton />
+          </div>
+        </CardFooter>
+      )}
+    </Card>
+  );
 }
