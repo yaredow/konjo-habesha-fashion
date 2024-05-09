cd2bce297bd444a381f5152778f22bf0525e7d94
yaredow <yaredyilma11@gmail.com>
2024-05-09 12:02:00 +0300

    add a function to fetch two factor token with token and email

--- src/data/twoFactorConfirmationToken.ts
+++ src/data/twoFactorConfirmationToken.ts
@@ -1,11 +1,22 @@
 import prisma from "@/lib/prisma";
 
-export const getTwoFactorConfirmationTokenWithUserId = async (
-  email: string,
-) => {
+export const getTwoFactorTokenByEmail = async (email: string) => {
   try {
     const twoFactorConfirmationToken = await prisma.twoFactorToken.findFirst({
       where: { email },
+    });
+
+    return twoFactorConfirmationToken;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getTwoFactorTokenByToken = async (token: string) => {
+  try {
+    const twoFactorConfirmationToken = await prisma.twoFactorToken.findUnique({
+      where: { token },
     });
 
     return twoFactorConfirmationToken;
