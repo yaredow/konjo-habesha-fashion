5cad6be79ac7ba55994bb2ac695f426e7262f698
yaredow <yaredyilma11@gmail.com>
2024-05-23 13:39:16 +0300

    add a link to a search page if the results length is greater than 6

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Search as SearchIcon } from "lucide-react";
+import { MoveRight, Search as SearchIcon } from "lucide-react";
 import useGetProductSearch from "@/utils/hook/useGetSearchProducts";
 import { UseMutateFunction } from "@tanstack/react-query";
 import Spinner from "./Spinner";
@@ -9,6 +9,7 @@ import { debounce } from "lodash";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { Product } from "@prisma/client";
+import { Input } from "./ui/input";
 
 type SearchType = {
   results: Product[] | null;
@@ -31,6 +32,8 @@ export default function Search() {
     if (value) {
       setOpen(true);
       debouncedSearch();
+    } else {
+      setOpen(false);
     }
   };
 
@@ -41,20 +44,18 @@ export default function Search() {
 
   return (
     <div className="relative w-full">
-      <div className="flex flex-1 items-center justify-between gap-4 rounded-md bg-gray-100 p-2">
-        <input
-          type="text"
-          name="name"
-          placeholder="Search"
-          value={query}
+      <div className="relative ml-auto flex-1 md:grow-0">
+        <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
           onChange={handleValueChange}
-          className="flex-1 rounded-md bg-transparent outline-none"
+          placeholder="Search..."
+          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
         />
-        <SearchIcon className="h-[20px] w-[20px] cursor-pointer" />
       </div>
 
       {open && (
-        <div className="absolute left-0 right-0 mt-2 w-full rounded-lg border border-gray-200 bg-white p-4 shadow-md">
+        <div className=" absolute left-0 right-0 mt-2 w-full rounded-lg border  bg-background p-4 shadow-md">
           {isPending && <Spinner />}
           {!isPending && results?.length === 0 && <div>No results found.</div>}
           {results?.length > 0 && (
@@ -71,9 +72,20 @@ export default function Search() {
                     src={result.images[0].url}
                     alt={result.images[0].public_id}
                   />
-                  <span>{result.name}</span>
+                  <span className="text-sm">{result.name}</span>
                 </div>
               ))}
+
+              {results?.length > 6 && (
+                <div className="mt-4 flex cursor-pointer items-center justify-between p-2 text-sm hover:bg-gray-100 ">
+                  <p>
+                    Search for <span className="italic">{query}</span>
+                  </p>
+                  <span>
+                    <MoveRight />
+                  </span>
+                </div>
+              )}
             </div>
           )}
         </div>
