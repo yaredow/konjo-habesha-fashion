2b2fea3d4638e23dadaa8ff7f92a96be28792f85
yaredow <yaredyilma11@gmail.com>
2024-06-02 17:15:03 +0300

    make the spinner center

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 export default function Spinner() {
   return (
-    <div className="h-10  w-10 animate-spin rounded-full border-4 border-gray-200 border-t-blue-500" />
+    <div className="grid h-10 w-10 animate-spin  items-center justify-center rounded-full border-4 border-gray-200 border-t-blue-500" />
   );
 }
