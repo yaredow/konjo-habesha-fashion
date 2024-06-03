dd8f3091e522a520eea1cd111103826923cbcff9
yaredow <yaredyilma11@gmail.com>
2024-06-03 12:33:50 +0300

    add margin to the browse all button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,17 @@ import TrendingProducts from "@/components/product/categories/TrendingProducts";
 
 export default function page() {
   return (
-    <div className=" flex flex-col gap-6 md:gap-16">
+    <div className=" flex flex-col md:gap-16">
       <>
         <Slider />
         <TrendingProducts />
         <FeaturedProducts />
       </>
 
-      <Button variant="secondary" className="mx-auto inline-block items-center">
+      <Button
+        variant="secondary"
+        className="mx-auto my-6 inline-block items-center"
+      >
         <Link href="/shop">Browse All</Link>
       </Button>
     </div>
