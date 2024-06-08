be2d83ca3f301997da88fa3dc5e81c74c51c4372
yaredow <yaredyilma11@gmail.com>
2024-06-08 15:04:24 +0300

    remove the generateStaticParam function

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import Spinner from "@/components/Spinner";
 import ProductDetail from "@/components/shop/ProductDetail";
-import { getAllProducts, getProduct } from "@/data/product";
-import { Product } from "@prisma/client";
+import { getProduct } from "@/data/product";
 import { Suspense } from "react";
 
 type Props = {
@@ -14,16 +13,6 @@ export async function generateMetadata({ params }: Props) {
   const product = await getProduct(slug);
 
   return { title: `${product?.name}` };
-}
-
-export async function generateStaticParams() {
-  const products: Product[] = await fetch(
-    "https://konjo-habesha-fashion.vercel.app//api/product/all",
-  ).then((res) => res.json());
-
-  return products?.map((product) => ({
-    slug: product.slug,
-  }));
 }
 
 export default async function Page() {
