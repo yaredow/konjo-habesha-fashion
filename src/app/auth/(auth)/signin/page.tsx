47e28fde088aa7c4991e36ddf3ca12850a42d630
yaredow <yaredyilma11@gmail.com>
2024-06-15 20:20:30 +0300

    add a metadata

--- src/app/auth/(auth)/signin/page.tsx
+++ src/app/auth/(auth)/signin/page.tsx
@@ -3,6 +3,37 @@ import CardWrapper from "@/components/auth/CardWrapper";
 import LoginForm from "@/components/forms/LoginForm";
 import { Suspense } from "react";
 
+export const metadata = {
+  title: "Sign In - Your E-commerce Site",
+  description:
+    "Sign in to your account on Your E-commerce Site to access your profile, orders, and more.",
+  keywords: ["e-commerce", "sign in", "login", "your site"],
+  authors: [
+    {
+      name: "Konjo Habesha Fashion",
+      url: "https://www.konjohasbeshafashion.com",
+    },
+  ],
+  openGraph: {
+    title: "Sign In - Your E-commerce Site",
+    description:
+      "Sign in to your account on Your E-commerce Site to access your profile, orders, and more.",
+    url: "https://www.your-ecommerce-site.com/signin",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@yoursite",
+    title: "Sign In - Your E-commerce Site",
+    description:
+      "Sign in to your account on Your E-commerce Site to access your profile, orders, and more.",
+    image: "https://www.your-ecommerce-site.com/images/og-image.jpg",
+  },
+  icons: {
+    icon: "/icon.png",
+  },
+};
+
 export default function Page() {
   return (
     <main className=" my-auto flex items-center justify-center">
